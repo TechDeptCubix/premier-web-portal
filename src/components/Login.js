3f29986ba128ff083e88ccff2bfb47286234f01d
TechDeptCubix <techdept@cubixitsolutions.com>
2021-05-29 17:23:55 +0400

    Something went wrong lost the progress of day one of Login page from master branch, but gh-pages with same code was present copied it, now committing

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,20 +2,44 @@ import React from "react";
 import "../css/Login.css"
 import logo from "../img/premier_logo.png";
 import backgroundImageLogin from "../img/backgroundImageLogin.png";
+import loginBgImage2 from "../img/login_bg_image-2.jpg";
 import pqpItem1 from "../img/pqpitem1.png";
 import brandlogoitem1 from "../img/mitsubishi_logo.png";
 import cubix_logo from "../img/cubix_logo.png";
 
+
+import fuso_logo from "../img/fuso_logo.png";
+import hyundai_logo from "../img/hyundai_logo.png";
+import Infiniti_logo from "../img/Infiniti_logo.png";
+import isuzu_logo from "../img/isuzu_logo.png";
+import lexus_logo from "../img/lexus_logo.png";
+import mazda_logo from "../img/mazda_logo.png";
+import nissan_logo from "../img/nissan_logo.png";
+import peugeot_logo from "../img/peugeot_logo.png";
+import renault_logo from "../img/renault_logo.png";
+import toyota_logo from "../img/toyota_logo.png";
+
+
+import part_1 from "../img/part_1.png";
+import part_2 from "../img/part_2.png";
+import part_3 from "../img/part_3.png";
+import part_4 from "../img/part_4.png";
+import part_5 from "../img/part_5.png";
+import part_6 from "../img/part_6.png";
+import part_7 from "../img/part_7.png";
+
 function Login(){
     return(
-        <div>
-            <div className="Login-logo-container"><img src={logo}/></div>
+        <div className="Login-page-height">
+            <div className="Login-logo-container"><img src={logo} className="Login-header-logo"/></div>
            
-           <div className="Login-main-section">
+            <div className="Login-main-section">
             
                 <div className="Login-bg-image-container">
-                    <img src={backgroundImageLogin} />
+                    <img src={backgroundImageLogin} className="Login-image-bg-first"/>
+                    <img src={loginBgImage2} className="Login-image-bg-second"/>
                 </div>
+
                 <div className="Login-main-section-content">
                     <h2>Welcome to Premier’s e-procurement</h2>
 
@@ -26,34 +50,39 @@ function Login(){
                     </form>
                     <div className="Login-pqp-items-carousel">
                         <ul>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
+                            <li><img src={part_1} /></li>
+                            <li><img src={part_2} /></li>
+                            <li><img src={part_3} /></li>
+                            <li><img src={part_4} /></li>
+                            <li><img src={part_5} /></li>
+                            <li><img src={part_6} /></li>
+                            <li><img src={part_7} /></li>
                         </ul>
                     </div>
 
-                    <div className="Login-brand-list-container">
-                    <ul>
-                            <li><img src={brandlogoitem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                            <li><img src={pqpItem1} /></li>
-                        </ul>
+                    <div className="Login-bottom-bar">
+                        <div className="Login-brand-list-container">
+                            <ul>
+                                <li><img src={brandlogoitem1} /></li>
+                                <li><img src={toyota_logo} /></li>
+                                <li><img src={fuso_logo} /></li>
+                                <li><img src={hyundai_logo} /></li>
+                                <li><img src={Infiniti_logo} /></li>
+                                <li><img src={isuzu_logo} /></li>
+                                <li><img src={lexus_logo} /></li>
+                                <li><img src={mazda_logo} /></li>
+                                <li><img src={nissan_logo} /></li>
+                                <li><img src={peugeot_logo} /></li>
+                                <li><img src={renault_logo} /></li>
+                            </ul>
+                        </div>
+                        <div className="Login-bottom-strip-content"> <span>powered by </span><img src={cubix_logo} /></div> 
                     </div>
-                </div>
+                </div> 
 
-            </div>
+            </div> 
 
-            <div className="Login-bottom-strip">
-                    <div> <span>powered by </span><img src={cubix_logo} /></div> 
-            </div>
+            
         </div>
     )
 }
